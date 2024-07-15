7a3af9eb7da5dbeee7e8fdcef52d0eb6f1915bf8
Denskerz <de.errr18@gmail.com>
2024-07-15 09:30:54 +0300

    Update request.py

--- request.py
+++ request.py
@@ -1,24 +1,6 @@
-в таблицу описанную моделью 
-class Model1(Base):
-    __tablename__ = "pledgeSecurities"
-    __table_args__ = {'extend_existing': True}
-
-    id: Mapped[intpk]
-    file_id: Mapped[int]
-    pledge_type_id: Mapped[int]
-    sb_id: Mapped[str_50]
-    security_name: Mapped[Optional[str]]
-    collateral_val_amt: Mapped[Optional[num_22_2]]
-    createdAt: Mapped[created_at]
-    updatedAt: Mapped[updated_at]
-
-записываю датафрейм с атрибутами
-file_id: Mapped[int]
-pledge_type_id: Mapped[int]
-sb_id: Mapped[str_50]
-security_name: Mapped[Optional[str]]   
-collateral_val_amt: Mapped[Optional[num_22_2]]
-
-через df.to_sql()
-возникает ошибка:
-"detail": "(psycopg2.errors.NotNullViolation) null value in column \"createdAt\" of relation \"commonAttributes\" violates not-null constraint
+        last_item = exec_query(conn_args=conn_args,
+                               sql_query=f'select * from "{settings.DB_SCHEMA}"."commonAttributes" order by id limit 1;')
+        if last_item.empty:
+            file_id = 0
+        else:
+            file_id = last_item["file_id"].squeeze()
