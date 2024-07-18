67a0bee5584f4445635fab5cedacabcd94a73647
Denskerz <de.errr18@gmail.com>
2024-07-18 03:16:00 -0700

    Update response.py

--- response.py
+++ response.py
@@ -1,1 +1,30 @@
-(cx_Oracle.DatabaseError) DPI-1047: Cannot locate a 64-bit Oracle Client library: "The specified module could not be found"
+from fastapi import FastAPI
+import asyncio
+
+app = FastAPI()
+
+# Определяем глобальные переменные, которые будут использоваться в приложении
+global_variable1 = None
+global_variable2 = None
+
+@app.on_event("startup")
+async def startup_event():
+    # Инициализируем глобальные переменные в фоновой задаче
+    await initialize_global_variables()
+
+async def initialize_global_variables():
+    global global_variable1, global_variable2
+
+    # Здесь вы можете выполнить любую необходимую логику для инициализации переменных
+    global_variable1 = "Значение 1"
+    global_variable2 = "Значение 2"
+
+    print("Глобальные переменные инициализированы")
+
+# Теперь вы можете использовать global_variable1 и global_variable2 в ваших маршрутах
+@app.get("/")
+def root():
+    return {
+        "global_variable1": global_variable1,
+        "global_variable2": global_variable2
+    }
