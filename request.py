1c1067e719a00c3740cf551b998a7ed0351453fa
Denskerz <de.errr18@gmail.com>
2024-08-13 16:04:20 +0300

    Update request.py

--- request.py
+++ request.py
@@ -1,1 +1,2 @@
-Error from server (BadRequest): error when creating "pvc.yaml": Pod in version "v1" cannot be handled as a Pod: strict decoding error: unknown field "spec.volumes[0].nfs.secretRef", unknown field "spec.volumes[0].nfs.user"
+
+kubectl get nodes -o jsonpath='{range .items[*]}{.metadata.name}{"\n"}{end}' | xargs -I {} ssh {} "apt-get list --installed | grep cifs-utils"
