b01feea1ae1711cf49f01addb4ad9a2e134ed411
Denskerz <de.errr18@gmail.com>
2024-08-13 10:32:42 +0300

    Update response.py

--- response.py
+++ response.py
@@ -1,18 +1,3 @@
-values
-
-persistentVolume:
-  name: cifs-pv
-  server: //SBERORM-WIN-IFT
-  shareName: CRZ-Test
-  secretName: cifs-secret
-  storageSize: 5Gi
-
-persistentVolumeClaim:
-  name: cifs-pvc
-  storageSize: 5Gi
-
-
-
 apiVersion: v1
 kind: PersistentVolume
 metadata:
@@ -22,46 +7,23 @@ spec:
     storage: {{ .Values.persistentVolume.storageSize }}
   accessModes:
   - ReadWriteOnce
-  cifs:
-    secretRef:
-      name: {{ .Values.persistentVolume.secretName }}
+  nfs:
     server: {{ .Values.persistentVolume.server }}
-    shareName: {{ .Values.persistentVolume.shareName }}
-    readOnly: false
+    path: {{ .Values.persistentVolume.shareName }}
     
-    
-    
-apiVersion: v1
-kind: PersistentVolumeClaim
-metadata:
-  name: {{ .Values.persistentVolumeClaim.name }}
-spec:
-  accessModes:
-  - ReadWriteOnce
-  resources:
-    requests:
-      storage: {{ .Values.persistentVolumeClaim.storageSize }}
-    
-    
-    
-    apiVersion: apps/v1
-kind: Deployment
-spec:
-  template:
-    spec:
-      containers:
-      - name: my-container
-        volumeMounts:
-        - name: cifs-volume
-          mountPath: /data
-      volumes:
-      - name: cifs-volume
-        persistentVolumeClaim:
-          claimName: {{ .Values.persistentVolumeClaim.name }}
-        
-        
-        
-        apiVersion: v1
+    persistentVolume:
+  name: cifs-pv
+  server: nfs-server.default.svc.cluster.local
+  shareName: /shared
+  secretName: nfs-secret
+  storageSize: 100Gi
+
+persistentVolumeClaim:
+  name: cifs-pvc
+  storageSize: 100Gi
+  
+  
+  apiVersion: v1
 kind: Secret
 metadata:
   name: {{ .Values.persistentVolume.secretName }}
@@ -69,4 +31,3 @@ type: Opaque
 stringData:
   username: {{ .Values.persistentVolume.username }}
   password: {{ .Values.persistentVolume.password }}
-  domain: {{ .Values.persistentVolume.domain }}
