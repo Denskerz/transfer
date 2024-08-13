0fdb99ce551b7d6043a3ac2b9cb8554453875657
Denskerz <de.errr18@gmail.com>
2024-08-13 15:43:17 +0300

    Update request.py

--- request.py
+++ request.py
@@ -1,6 +1,1 @@
-  Warning  FailedMount  8m56s (x2 over 36m)   kubelet            Unable to attach or mount volumes: unmounted volumes=[data], unattached volumes=[kube-api-access-k5bvh data]: timed out waiting for the condition
-  Warning  FailedMount  3m30s (x14 over 42m)  kubelet            MountVolume.SetUp failed for volume "data" : mount failed: exit status 32
-Mounting command: mount
-Mounting arguments: -t nfs 172.30.56.144:/C:/CRZ-Test /var/lib/kubelet/pods/6bf07a7f-a2d3-4890-91c0-a654bfb939a3/volumes/kubernetes.io~nfs/data
-Output: mount.nfs: Connection refused
-  Warning  FailedMount  2m12s (x17 over 42m)  kubelet  Unable to attach or mount volumes: unmounted volumes=[data], unattached volumes=[data kube-api-access-k5bvh]: timed out waiting for the condition
+Error from server (BadRequest): error when creating "pvc.yaml": Pod in version "v1" cannot be handled as a Pod: strict decoding error: unknown field "spec.volumes[0].nfs.secretRef", unknown field "spec.volumes[0].nfs.user"
