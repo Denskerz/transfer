7d196cf79f6c18fa397d268dd01f47e28a2b2f7c
Denskerz <de.errr18@gmail.com>
2024-08-13 10:59:50 +0300

    Update response.py

--- response.py
+++ response.py
@@ -1,33 +1,5 @@
-apiVersion: v1
-kind: PersistentVolume
-metadata:
-  name: {{ .Values.persistentVolume.name }}
-spec:
-  capacity:
-    storage: {{ .Values.persistentVolume.storageSize }}
-  accessModes:
-  - ReadWriteOnce
-  nfs:
-    server: {{ .Values.persistentVolume.server }}
-    path: {{ .Values.persistentVolume.shareName }}
-    
-    persistentVolume:
-  name: cifs-pv
-  server: nfs-server.default.svc.cluster.local
-  shareName: /shared
-  secretName: nfs-secret
-  storageSize: 100Gi
-
-persistentVolumeClaim:
-  name: cifs-pvc
-  storageSize: 100Gi
-  
-  
-  apiVersion: v1
-kind: Secret
-metadata:
-  name: {{ .Values.persistentVolume.secretName }}
-type: Opaque
-stringData:
-  username: {{ .Values.persistentVolume.username }}
-  password: {{ .Values.persistentVolume.password }}
+  volumes:
+    crz:
+      mountPath: opt/server/archives
+      server: 172.30.56.144
+      path: /share
