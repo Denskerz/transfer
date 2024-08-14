78e4761f692a47566b8b4b44a3656440cec3b28f
Denskerz <de.errr18@gmail.com>
2024-08-14 10:04:19 +0300

    Update request.py

--- request.py
+++ request.py
@@ -1,22 +1,10 @@
-import requests
-import json
-from datetime import datetime, timedelta
+FROM 172.30.71.8:5001/images/ubi9-python311:latest
 
-# Получаем текущую дату и предыдущую дату
-current_date = datetime.now().date()
-previous_date = current_date - timedelta(days=1)
+USER root
 
-# Формируем тело запроса в формате JSON
-request_body = {
-    "start_date": str(previous_date),
-    "end_date": str(current_date)
-}
 
-# Отправляем POST-запрос с телом запроса
-response = requests.post("http://0.0.0.0:8040/start", data=json.dumps(request_body), headers={"Content-Type": "application/json"})
+COPY ./pip.conf /etc/pip.conf
+COPY . .
 
-# Проверяем статус ответа
-if response.status_code == 200:
-    print("Запрос успешно отправлен")
-else:
-    print(f"Ошибка при отправке запроса: {response.status_code} - {response.text}")
+RUN microdnf install -y cifs-utils
+RUN pip3 install -r ./requirements.txt
