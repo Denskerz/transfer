f3a55b9778957a650fcfc717570f2704d2d1265a
Denskerz <de.errr18@gmail.com>
2024-08-14 10:42:13 +0300

    Update request.py

--- request.py
+++ request.py
@@ -1,5 +1,25 @@
-STEP 5/6: RUN dnf install -y cifs-utils
-Updating Subscription Management repositories.
-Unable to read consumer identity
+apiVersion: v1  
+kind: ConfigMap
+metadata:
+  name: cifs-mount
+data:
+  mount-options: 'username=eroshevich_d,password=12345678,workgroup=sigma-belpsb.by'
 
-This system is not registered with an entitlement server. You can use subscription-manager to register.
+  volumeMounts:
+  - name: cifs-mount  
+    mountPath: /mount/path
+  env:
+  - name: MOUNT_OPTIONS
+    valueFrom:
+      configMapKeyRef:
+        name: cifs-mount 
+        key: mount-options
+        
+volumes:
+- name: cifs-mount
+  cifs:
+    server: //SBERORM-WIN-IFT/CRZ-Test
+    path: /mount/path
+    securityStyle: unix
+    volumeOptions:
+      opect: {{.data.MOUNT_OPTIONS}}  
