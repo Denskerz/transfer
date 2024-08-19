84c30b142a53c16cfc80d628a9bfe15f0758154c
Denskerz <de.errr18@gmail.com>
2024-08-19 16:42:27 +0300

    Update request.py

--- request.py
+++ request.py
@@ -1,1 +1,5 @@
 https://github.com/dsuch/pymqi/tree/main/code
+
+Alternatively, for version 18 and earlier, every shell running Python will need to have the environment variable LD_LIBRARY_PATH set to the appropriate directory for the Instant Client version. For example:
+export LD_LIBRARY_PATH=/usr/lib/oracle/18.5/client64/lib:$LD_LIBRARY_PATH
+как это сделать и запустить питон скрипт из консоли
