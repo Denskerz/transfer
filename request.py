c11ca8207b50f28f1164f61238393a7150a31e2f
Denskerz <de.errr18@gmail.com>
2024-08-19 17:55:29 +0300

    Update request.py

--- request.py
+++ request.py
@@ -1,5 +1,9 @@
 https://github.com/dsuch/pymqi/tree/main/code
 
-Alternatively, for version 18 and earlier, every shell running Python will need to have the environment variable LD_LIBRARY_PATH set to the appropriate directory for the Instant Client version. For example:
-export LD_LIBRARY_PATH=/usr/lib/oracle/18.5/client64/lib:$LD_LIBRARY_PATH
-как это сделать и запустить питон скрипт из консоли
+If you use optional Oracle configuration files such as tnsnames.ora, sqlnet.ora or oraaccess.xml with Instant Client, then put the files in an accessible directory, for example in /opt/oracle/your_config_dir. Then use:
+
+import cx_Oracle
+cx_Oracle.init_oracle_client(config_dir="/opt/oracle/your_config_dir")
+Or set the environment variable TNS_ADMIN to that directory name.
+
+Alternatively, put the files in the network/admin subdirectory of Instant Client, for example in /usr/lib/oracle/21/client64/lib/network/admin. This is the default Oracle configuration directory for executables linked with this Instant Client.
