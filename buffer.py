2ea976363a1956be09082c9637093746d2974ad7
Denskerz <de.errr18@gmail.com>
2024-08-27 01:28:20 -0700

    Update buffer.py

--- buffer.py
+++ buffer.py
@@ -1,17 +1,67 @@
-обьясни подробно
-После установки кластера понадобится установить ipa-server или реквизиты от уже существующего,
-все сервера (все hadoop ноды, opensearh ноды и сервера с компонентами ГП) должны быть подключены
-к этому инстансу ipa:
-sudo ipa-client-install --hostname='graf-ift-h4.hdp.test' --realm='HDP.TEST' --domain='hdp.test' --
-server='ipa.hdp.test' --force-join
-Для каждой ноды должны быть A и PTR записи, зону можно создать в freeipa (тогда нужно указывать ее
-в качестве DNS сервера на нодах)
-После запуска ambari-server необходимо подключить хосты hadoop кластера, импортировать stack
-компонентов hadoop и начать поэтапную установку сервисов.
-В первую очередь ставится zookeeper, во вторую hdfs и после его установки нужно из меню ACTIONS
-компонента запустить и установить HA mode для компонента.
-В третью очередь ставятся yarn и зависимости, после установки yarn тоже нужно настроить в HA
-mode.
-Далее поочередно или все вместе устанавливаются остальные компоненты hadoop.
-Установку kerberos можно запустить или сразу после установки HA mode yarn или в конце, после
-установки всех сервисов.
+import os
+import schedule
+import time
+import paramiko
+import base64
+import logging
+from subprocess import check_output, CalledProcessError
+
+# SSH connection details
+SSH_HOST = "172.30.56."
+SSH_USER = "graf_user"
+SSH_PASSWORD = "graf_pao_user"
+
+encoded_user = base64.b64encode(SSH_USER.encode()).decode()
+encoded_password = base64.b64encode(SSH_PASSWORD.encode()).decode()
+
+kinit_command = "kinit -kt ~/admin.keytab admin@HDP.TEST"
+kinit_args = kinit_command.split(' ')
+
+# Logging configuration
+logging.basicConfig(
+    filename='scheduled_kinit.log',
+    level=logging.INFO,
+    format='%(asctime)s %(levelname)s: %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S'
+)
+
+host_end_list = ["46", "47", "48", "49", "50"]
+
+
+def run_kinit():
+    try:
+        output = check_output(kinit_args, universal_newlines=True)
+        logging.info("kinit command executed.")
+        print(output)
+    except CalledProcessError as e:
+        logging.error(f"Error running kinit: {e}")
+
+    for host_end in host_end_list:
+        host = SSH_HOST + hostend
+        try:
+            # Establish SSH connection
+            ssh = paramiko.SSHClient()
+            ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+            ssh.connect(host, username=SSH_USER, password=SSH_PASSWORD)
+
+            # Execute the kinit command on the remote server
+            stdin, stdout, stderr = ssh.exec_command(kinit_command)
+            output = stdout.read().decode().strip()
+            error = stderr.read().decode().strip()
+            if error:
+                logging.error(f"Server Error executing kinit on the {host}: {error}")
+            else:
+                logging.info(f"kinit command executed on the {host}: {output}")
+
+            # Close the SSH connection
+            ssh.close()
+        except Exception as e:
+            logging.error(f"Error connecting to the remote server {host}: {e}")
+
+# Scheduling the script to run daily at 12:00 AM (midnight)
+schedule.every(1).minutes.do(run_kinit)
+
+while True:
+    schedule.run_pending()
+    time.sleep(30)
+
