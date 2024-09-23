2ffc7c05b4be8d5874c4d1043431486f7f608b69
Denskerz <de.errr18@gmail.com>
2024-09-23 14:12:36 +0300

    Update buffer.py

--- buffer.py
+++ buffer.py
@@ -1,71 +1,35 @@
-apiVersion: v1
-kind: Secret
+apiVersion: apps/v1
+kind: Deployment
 metadata:
-  name: pv-creds-cifs-bzi
-  namespace: kube-system
-type: Opaque
-data:
-  username: QlppX1RlY2g=
-  password: MjMzNDU2Nzg=
-  
-  
-  persistentVolume и persistentVolumeClaim уже прописаны в кубере
-persistentVolume:
-apiVersion: v1
-kind: PersistentVolume
-metadata:
-  finalizers:
-  - kubernetes.io/pv-protection
-  name: pv-smb-bzi-crz-dispatcher
+  name: my-deployment
 spec:
-  accessModes:
-  - ReadWriteMany
-  capacity:
-    storage: 200Gi
-  claimRef:
-    apiVersion: v1
-    kind: PersistentVolumeClaim
-    name: pvc-smb-bzi-crz-dispatcher
-    namespace: bzi
-  csi:
-    driver: smb.csi.k8s.io
-    nodeStageSecretRef:
-      name: pv-creds-cifs-bzi
-      namespace: kube-system
-    volumeAttributes:
-      source: //pdc-psi.belpsb.by/crz-dispatcher
-    volumeHandle: cifs-csi-bzi-crz-dispatcher
-  mountOptions:
-  - dir_mode=0755
-  - file_mode=0644
-  - uid=1001
-  - gid=1001
-  - noperm
-  - mfsymlinks
-  - cache=strict
-  - noserverino
-  - vers=3.0
-  persistentVolumeReclaimPolicy: Retain
-  storageClassName: smb
-  volumeMode: Filesystem
-
-persistentVolumeClaim:
-apiVersion: v1
-kind: PersistentVolumeClaim
-metadata:
-  finalizers:
-  - kubernetes.io/pvc-protection
-  name: pvc-smb-bzi-crz-dispatcher
-  namespace: bzi
-spec:
-  accessModes:
-  - ReadWriteMany
-  resources:
-    requests:
-      storage: 200Gi
-  storageClassName: smb
-  volumeMode: Filesystem
-  volumeName: pv-smb-bzi-crz-dispatcher
-
-как теперь в моих хелмчартах прописать secret с user: BZI_Tech и password: 12345678, чтобы использовать pvc?
-
+  replicas: 1
+  selector:
+    matchLabels:
+      app: my-app
+  template:
+    metadata:
+      labels:
+        app: my-app
+    spec:
+      containers:
+      - name: my-container
+        image: my-image
+        volumeMounts:
+        - mountPath: /data
+          name: my-storage
+        env:
+        - name: DB_USERNAME
+          valueFrom:
+            secretKeyRef:
+              name: my-secret
+              key: username
+        - name: DB_PASSWORD
+          valueFrom:
+            secretKeyRef:
+              name: my-secret
+              key: password
+      volumes:
+      - name: my-storage
+        persistentVolumeClaim:
+          claimName: my-pvc
