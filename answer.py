6a3f7af4e2d11320aa45b7771d144b1668fe364a
Denskerz <de.errr18@gmail.com>
2024-10-02 11:58:37 +0300

    Update answer.py

--- answer.py
+++ answer.py
@@ -20,7 +20,12 @@ pipeline {
         }
     }
 }
-
+script {
+    def tables = params.TABLE_NAMES.readLines().findAll { it }
+    echo "Parsed tables: ${tables}"
+    def tablesString = tables.collect { "\"${it}\"" }.join(', ')
+    sh "ansible-playbook playbook.yml -e 'tables=[${tablesString}]'"
+}
 
 Also:   org.jenkinsci.plugins.workflow.actions.ErrorAction$ErrorId: 76c8f737-8aea-4847-9601-7f3ad70cc15f
 hudson.remoting.ProxyException: groovy.lang.MissingMethodException: No signature of method: java.util.ArrayList.collect() is applicable for argument types: (org.codehaus.groovy.runtime.GStringImpl) values: ["null"]
