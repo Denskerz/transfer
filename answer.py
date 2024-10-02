e848beb3201ce1eeb41c893a90158a0a903d48fb
Denskerz <de.errr18@gmail.com>
2024-10-02 17:18:56 +0300

    Update answer.py

--- answer.py
+++ answer.py
@@ -1,32 +1,16 @@
-pipeline {
-    agent any
+---
+- name: Изменение содержимого файла
+  hosts: localhost
+  tasks:
+    - name: Добавить строку в начало файла
+      lineinfile:
+        path: /path/to/your/file
+        line: 'model_path = "/path/to/model"'
+        state: present
+        insertbefore: BOF  # Вставляет строку в начало файла
 
-    parameters {
-        string(name: 'TABLE_NAMES', defaultValue: 'table1', description: 'Enter table names separated by new lines')
-    }
-
-    stages {
-        stage('Run Ansible Playbook') {
-            steps {
-                script {
-                    // Разделение строк на массив и преобразование в нужный формат
-                    def tables = params.TABLE_NAMES.readLines()
-                    def tablesString = tables.collect { "\"${it}\"" }.join(', ')
-                    
-                    // Вызов Ansible Playbook с передачей переменной
-                    sh "ansible-playbook playbook.yml -e 'tables=[${tablesString}]'"
-                }
-            }
-        }
-    }
-}
-script {
-    def tables = params.TABLE_NAMES.readLines().findAll { it }
-    echo "Parsed tables: ${tables}"
-    def tablesString = tables.collect { "\"${it}\"" }.join(', ')
-    sh "ansible-playbook playbook.yml -e 'tables=[${tablesString}]'"
-}
-
-Also:   org.jenkinsci.plugins.workflow.actions.ErrorAction$ErrorId: 76c8f737-8aea-4847-9601-7f3ad70cc15f
-hudson.remoting.ProxyException: groovy.lang.MissingMethodException: No signature of method: java.util.ArrayList.collect() is applicable for argument types: (org.codehaus.groovy.runtime.GStringImpl) values: ["null"]
-Possible solutions: collect(), collect(), collect(groovy.lang.Closure), collect(groovy.lang.Closure), collect(java.util.Collection, groovy.lang.Closure), collect(java.util.Collection, groovy.lang.Closure)
+    - name: Заменить все строки ".." на "{model_path}"
+      replace:
+        path: /path/to/your/file
+        regexp: '^\.\.$'  # Регулярное выражение для строки ".."
+        replace: '{model_path}'
