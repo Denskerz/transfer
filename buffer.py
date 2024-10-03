18ed2742b6e698a6b884c3f0367fffe152ff418b
Denskerz <de.errr18@gmail.com>
2024-10-03 17:22:32 +0300

    Update buffer.py

--- buffer.py
+++ buffer.py
@@ -1,23 +1,13 @@
-#!/bin/bash
+#!/usr/bin/expect
 
-# Prompt for the folder path
-read -p "Enter the path to the folder for archiving: " folder_path
+set timeout -1
 
-# Prompt for the remote server address
-read -p "Enter the remote server address: " remote_server
+# Ваша команда
+spawn your_command
 
-# Prompt for the folder path on the remote server to save the archive
-read -p "Enter the path to the folder on the remote server to save the archive: " remote_folder
+# Ожидание запроса пароля
+expect "Password:"
+send "your_password\r"
 
-# Archive the folder and split into 10 parts
-ssh "$remote_server" "tar -czf - '$folder_path' | split -b 10M - '${remote_folder}/archive_part_'"
-
-# Exit from the remote server (ssh will automatically close after the command)
-
-# Copy the necessary files to Windows
-# Make sure you have WinSCP or another SCP client installed
-read -p "Enter the path to the files on the remote server to copy: " remote_files_path
-read -p "Enter the path on your computer to save the files: " local_save_path
-
-# Use WinSCP to copy files
-winscp.com /command "open sftp://$remote_server" "get '$remote_files_path/*' '$local_save_path'" "exit"
+# Ожидание завершения команды
+expect eof
