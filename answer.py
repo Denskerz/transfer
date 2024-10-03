6459dd759856d2f4d284160e775252df31c5e4f2
Denskerz <de.errr18@gmail.com>
2024-10-03 09:46:02 +0300

    Update answer.py

--- answer.py
+++ answer.py
@@ -1,17 +1,20 @@
 ---
 - name: Изменение содержимого файла
   hosts: localhost
+  vars:
+    workdir: /path/to/your  # Задайте значение переменной workdir здесь
+
   tasks:
     - name: Добавить строку в начало файла
       lineinfile:
-        path: /path/to/your/file
+        path: "{{ workdir }}/file"
         line: 'model_path = "/path/to/model"'
         state: present
         insertbefore: BOF  # Вставляет строку в начало файла
 
     - name: Заменить строки с использованием цикла
       replace:
-        path: /path/to/your/file
+        path: "{{ workdir }}/file"
         regexp: "{{ item.regexp }}"
         replace: "{{ item.replace }}"
       loop:
@@ -20,10 +23,15 @@
         - { regexp: '«,', replace: '«.' }
         - { regexp: '^input_filepath.*$', replace: '«hello»' }  # Заменяет строку input_filepath на «hello»
 
-    - name: Добавить строку input_filepath, если её нет
-      lineinfile:
-        path: /path/to/your/file
-        line: 'input_filepath'
-        state: present
-      when: >
-        not (lookup('file', '/path/to/your/file').find('input_filepath') != -1)
+    - name: Обработать input_filepath и output_filepath
+      block:
+        - name: Добавить строку {{ item }} если её нет
+          lineinfile:
+            path: "{{ workdir }}/file"
+            line: "{{ item.line }}"
+            state: present
+          when: >
+            not (lookup('file', '{{ workdir }}/file').find(item.check) != -1)
+          loop:
+            - { line: 'input_filepath = f"../shshsj"', check: 'input_filepath = f"../shshsj"' }
+            - { line: 'output_filepath = f"../output"', check: 'output_filepath = f"../output"' }
