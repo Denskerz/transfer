63c81fac5218df195629a7fe65d79b2aea734e94
Denskerz <de.errr18@gmail.com>
2024-10-10 07:14:26 -0700

    Update response.py

--- response.py
+++ response.py
@@ -1,5 +1,1 @@
-Chain CNI-DN-ced8a516105815b5bdb6a (1 references)
- pkts bytes target     prot opt in     out     source               destination
-    0     0 CNI-HOSTPORT-SETMARK  tcp  --  *      *       10.88.0.0/16         0.0.0.0/0            tcp dpt:7110
-   16   960 CNI-HOSTPORT-SETMARK  tcp  --  *      *       127.0.0.1            0.0.0.0/0            tcp dpt:7110
-   18  1080 DNAT       tcp  --  *      *       0.0.0.0/0            0.0.0.0/0            tcp dpt:7110 to:10.88.0.153:7110
+sudo iptables -t nat -A OUTPUT -p tcp --dport 80 -j REDIRECT --to-ports 7110 
