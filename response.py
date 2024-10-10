c841c0d6daf939f038f4808aa9850ab69ca4ded8
Denskerz <de.errr18@gmail.com>
2024-10-10 17:35:39 +0300

    Update response.py

--- response.py
+++ response.py
@@ -1,2 +1,1 @@
-# Убедитесь, что iptables установлен
-sudo iptables -t nat -A POSTROUTING -p tcp --dport 80 -j SNAT --to-source <IP-хоста>:7110
+curl -L --insecure -x 172.30.71.80:3128 -X POST 'http://172.30.71.10:50510/api/v2/oauth' \ -H 'Content-Type: application/x-www-form-urlencoded' \ -H 'Accept: application/json' \ -H 'RqUID: 6f0b1291-c7f3-43c6-bb2e-9f3efb2dc98e' \ -H 'Authorization: Basic OGJiZTg3NGEtNjA0OS00NjQ3LTk1YWMtNzVhNDNjNzdmODUxOjU0MTY1OWNhLWI3NDctNDBlZS05MTM3LWY3Y2M0OGY3NWQ4Zg==' \ --data-urlencode 'scope=GIGACHAT_API_CORP'
