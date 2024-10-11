3979195adaf24d17bf896b109790777cc41bda08
Denskerz <de.errr18@gmail.com>
2024-10-11 16:44:12 +0300

    Update request.py

--- request.py
+++ request.py
@@ -4,3 +4,28 @@ curl -L --insecure -x 172.30.71.80:3128 -X POST 'http://172.30.71.10:50510/api/v
 -H 'RqUID: 6f0b1291-c7f3-43c6-bb2e-9f3efb2dc98e' \
 -H 'Authorization: Basic OGJiZTg3NGEtNjA0OS00NjQ3LTk1YWMtNzVhNDNjNzdmODUxOjU0MTY1OWNhLWI3NDctNDBlZS05MTM3LWY3Y2M0OGY3NWQ4Zg==' \
 --data-urlencode 'scope=GIGACHAT_API_CORP'
+
+
+curl -L --insecure -x 172.30.71.80:3128 -X POST 'http://172.30.71.10:50511/api/v1/chat/completions' \
+-H 'Content-Type: application/json' \
+-H 'Accept: application/json' \
+-H 'Authorization: 'Bearer взять из oauth' \
+--data-raw '{
+  "model": "GigaChat",
+  "messages": [
+    {
+      "role": "system",
+      "content": "Ты опытный специалит контакт-центра Банка"
+    },
+    {
+      "role": "user",
+      "content": " 12082024 14082024 "
+    }
+  ],
+  "temperature":1,
+  "top_p":0.1,
+  "n": 1,
+  "stream": false,
+  "max_tokens":100,
+  "update_interval": 0
+}'
