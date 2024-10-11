cf2d7f064089fe7abe43ec9f38d1e0856c40c714
Denskerz <de.errr18@gmail.com>
2024-10-11 05:22:19 -0700

    Update request.py

--- request.py
+++ request.py
@@ -1,11 +1,1 @@
-09:40:20  + podman build -f docker/Dockerfile --format=docker --no-cache --pull-always -t nexus3-ift.sigma-belpsb.by:5001/images/gigachat_rag_service:202410110929 .
-09:40:20  STEP 1/10: FROM nexus3-ift.sigma-belpsb.by:5001/images/ubi9-python311:latest
-09:40:20  Trying to pull nexus3-ift.sigma-belpsb.by:5001/images/ubi9-python311:latest...
-09:40:21  Getting image source signatures
-09:40:21  Copying blob sha256:b7af5bed306dcea702d787c2b9c02d07d773742500c0213475289f7837cc0fcd
-09:40:21  Copying blob sha256:c20470d0be35939741977f0ca184f1072d55e7d5cff1e9c4d7cb9baa9de9c9fb
-09:40:21  Copying blob sha256:fcb3b197a73b700abaa9ba089b6e96d570365ce9ff0e4b355ba04c4b5fd8a590
-09:40:21  Copying blob sha256:edab65b863aead24e3ed77cea194b6562143049a9307cd48f86b542db9eecb6e
-09:40:21  Copying config sha256:6bd2491384b098e2d175f9452aab07b9ffa23ca05ca937e1f02d6959cb11fc7e
-09:40:21  Writing manifest to image destination
-09:40:21  Error: creating build container: creating container: creating read-write layer with ID "97bfd7831bb37a6c18aee7afa414ae8e32479788546d41119bbc57222644e300": Stat /var/lib/jenkins/.local/share/containers/storage/overlay/5669cc385d32c72282366710f375edf0559086f8f0474c52715b9fabe2036503/diff: no such file or directory
+### OAUTH curl -L --insecure -x 172.30.71.80:3128 -X POST 'http://172.30.71.10:50510/api/v2/oauth' \ -H 'Content-Type: application/x-www-form-urlencoded' \ -H 'Accept: application/json' \ -H 'RqUID: 6f0b1291-c7f3-43c6-bb2e-9f3efb2dc98e' \ -H 'Authorization: Basic OGJiZTg3NGEtNjA0OS00NjQ3LTk1YWMtNzVhNDNjNzdmODUxOjU0MTY1OWNhLWI3NDctNDBlZS05MTM3LWY3Y2M0OGY3NWQ4Zg==' \ --data-urlencode 'scope=GIGACHAT_API_CORP'   ### Others curl -L --insecure -x 172.30.71.80:3128 -X POST 'http://172.30.71.10:50511/api/v1/chat/completions' \ -H 'Content-Type: application/json' \ -H 'Accept: application/json' \ -H 'Authorization: 'Bearer взять из oauth' \ --data-raw '{   "model": "GigaChat",   "messages": [     {       "role": "system",       "content": "Ты опытный специалит контакт-центра Банка"     },     {       "role": "user",       "content": " 12082024 14082024 "     }   ],   "temperature":1,   "top_p":0.1,   "n": 1,   "stream": false,   "max_tokens":100,   "update_interval": 0 }'
