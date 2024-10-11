47b31400dd6d4e73048eb676e262dd7ac0a3d4f8
Denskerz <de.errr18@gmail.com>
2024-10-11 13:50:03 +0300

    Update request.py

--- request.py
+++ request.py
@@ -1,64 +1,10 @@
-import pymqi
-import json
-import pandas as pd
-
-# Параметры подключения
-HOST = 'mb-gw3psifs.belpsb.by'
-PORT = 1414
-CHANNEL = 'BZI.SVRCONN'
-QUEUE_MANAGER = 'MBY.ESB.AC0.GW3'
-QUEUE_NAME = 'ESB.BZI.REQUEST'
-USER = 'bziusr'
-PASSWORD = 'your_password_here'
-
-# Создание контекста соединения
-qmgr = pymqi.QueueManager(None)
-cd = pymqi.CD()
-cd.ChannelName = CHANNEL
-cd.ConnectionName = f'{HOST}({PORT})'
-cd.UserIdentifier = USER
-cd.Password = PASSWORD
-qmgr.connect_with_options(QUEUE_MANAGER, cd)
-
-# Создание очереди
-queue = pymqi.Queue(qmgr, QUEUE_NAME)
-
-# Получение сообщения
-message = queue.get()
-
-# Разбор полученного сообщения
-data = json.loads(message)
-url = data['url']
-
-# Получение датафрейма из JSON-файла
-df = pd.read_json(url)
-
-# Закрытие соединения
-queue.close()
-qmgr.disconnect()
-
-# Работа с полученным датафреймом
-print(df)
-
-
-есть некоторые данные, которые могут помочь для подключения к очереди IBM-MQ:
-
-AdapterCURS; 20240813_1250
-AppDirectoryData; 20240813_1433
-AdapterBZI.20240603_1245
-для ПСИ\ПРОМа в домене BELPSB зарегистрирован пользователь bziusr и группа bzimqi
-пользователь bziusr добавлен в группу mqmmqi и bzimqi.
-параметры подключения к КСШ на ПСИ для АС "База залогового имущества" :
-хост: mb-gw3psifs.belpsb.by
-порт 1414
-менеджер: MBY.ESB.AC0.GW3
-канал BZI.SVRCONN
-шифрование TLS_RSA_WITH_AES_128_CBC_SHA256
-SSL ключи те же что и для ИФТ контура
-Очередь для получения нотификаций (в том числе sendDirectoryDataNf): ESB.BZI.REQUEST
-
-суть задачи:
-необходимо написать подключение к очереди ibm-mq на python, мой сервис будет ждать сообщение, которое мне отправят, в сообщении будет ссылка на json файл, из которого необходимо будет забрать датафрейм 
-
-
-https://practicum.yandex.ru/data-analyst/
+10:34:14  + podman build -f docker/Dockerfile --format=docker --no-cache --pull-always -t nexus3-ift.sigma-belpsb.by:5001/images/gigachat_rag_service:202410111022 .
+10:34:14  STEP 1/10: FROM nexus3-ift.sigma-belpsb.by:5001/images/ubi8-python38:latest
+10:34:15  Trying to pull nexus3-ift.sigma-belpsb.by:5001/images/ubi8-python38:latest...
+10:34:15  Getting image source signatures
+10:34:15  Copying blob sha256:68be66ceca368ebe4a844b1821250d8d8c95fcdaa10e7699e9b378fedb47f798
+10:34:15  Copying blob sha256:55e827cc6c85bbf96b303b2a1d283a64a0749752b2ffbce233c3578036ebfeb6
+10:34:15  Copying blob sha256:0dbe531b0d7b1c6b3a4e24bad4cecdf2edc9ba16351a58879559723e453e63d9
+10:34:15  Copying config sha256:ce7f5461dc494d9fa52e0ef2aa951a567fb7323f6be16405e5cba2fece67eb59
+10:34:15  Writing manifest to image destination
+10:34:15  Error: creating build container: creating container: creating read-write layer with ID "da1f45f1c12f0a6ca38e08608bc42b52956aee391022e7897448c05989715697": Stat /var/lib/jenkins/.local/share/containers/storage/overlay/b8c12644dfea82fa64fc5a3c3e926302d7bf7399fd237d17ae72ff28f6f8f380/diff: no such file or directory
