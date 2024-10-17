efe551e9ac40bfbd86de58b901ebbf9be4a53f05
Denskerz <de.errr18@gmail.com>
2024-10-17 09:36:21 +0300

    Update response.py

--- response.py
+++ response.py
@@ -1,1 +1,2 @@
 Тестирование и развертка моделей в производственную среду; конфигурация серверной инфраструктуры; поддержка сервиса для микросервисного приложения "База залогового имущества"
+teradatasql.OperationalError: [Version 17.20.0.31] [Session 57461646] [Teradata Database] [Error 2644] SP_MOVE_PRED_TO_CODM:No more room in database PROD_DBML.
